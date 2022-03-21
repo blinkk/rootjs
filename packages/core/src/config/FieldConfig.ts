198b5784cdd9a1271441e42b6e1429e769cea481
Randy Merrill <randy@blinkk.com>
2022-03-21 09:34:16 -0600

    Updating comments for field configuration.

--- packages/core/src/config/FieldConfig.ts
+++ packages/core/src/config/FieldConfig.ts
@@ -3,21 +3,19 @@
  */
 export interface BaseFieldConfig {
   /**
-   * default value to use for the field when no value is provided.
+   * Default value to use for the field when no value is provided.
    */
   default?: unknown;
   /**
    * Help string to assist user in understanding the expectations of the field.
-   *
-   * In complex fields, this can be broken up into zones.
    */
-  help?: string | Record<string, string>;
+  help?: string;
   /**
    * Label for the field in the editor.
    */
   label?: string;
   /**
-   * Type of field. Used to create the correct field in the editor UI.
+   * Type of field.
    */
   type: string;
 }
