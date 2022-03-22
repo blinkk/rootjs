a9dd06d304b7ccb0780cc8f6924309979d3f1797
Steven Le <stevenle08@gmail.com>
2022-03-22 06:38:57 -0700

    feat: add dev script at project root

--- packages/core/src/commands/dev.ts
+++ packages/core/src/commands/dev.ts
@@ -1,10 +1,19 @@
+import * as path from 'path';
 import {cwd, env} from 'process';
 import {Server} from '../server';
 import Workspace from '../workspace/Workspace';
 
 export async function dev(dirPath?: string) {
   // TODO(stevenle): load dotenv files.
-  const workspaceDir = dirPath || env.CMS_WORKSPACE || cwd();
+  let workspaceDir = dirPath || env.CMS_WORKSPACE;
+  if (workspaceDir) {
+    if (!workspaceDir.startsWith('/')) {
+      // Resolve relative paths.
+      workspaceDir = path.resolve(cwd(), workspaceDir);
+    }
+  } else {
+    workspaceDir = cwd();
+  }
   const workspace = await Workspace.init(workspaceDir);
   const server = new Server({
     workspace: workspace,
