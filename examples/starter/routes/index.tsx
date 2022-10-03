45fe02b65d249f15ab7a2890349448b623e3f661
Steven Le <stevenle08@gmail.com>
2022-10-03 15:23:17 -0700

    fix: fix starter example

--- examples/starter/routes/index.tsx
+++ examples/starter/routes/index.tsx
@@ -1,7 +1,8 @@
 import {BaseLayout} from '@/layouts/base';
-import {t} from '@blinkk/root';
+import {useTranslations} from '@blinkk/root';
 
 export default function Page() {
+  const t = useTranslations();
   return (
     <BaseLayout title="Welcome to Root.js!">
       <h1>{t('Hello world!')}</h1>
