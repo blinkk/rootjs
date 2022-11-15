b2f6ff84ff830b7447f33af477d9bb895243876e
Steven Le <stevenle08@gmail.com>
2022-11-14 22:05:52 -0800

    feat: print output file sizes

---
+++ .changeset/brown-tigers-bathe.md
@@ -0,0 +1,5 @@
+---
+'@blinkk/root': patch
+---
+
+feat: print output file sizes
--- packages/root/src/cli/commands/build.ts
+++ packages/root/src/cli/commands/build.ts
@@ -7,7 +7,6 @@ import {pluginRoot} from '../../render/vite-plugin-root.js';
 import {BuildAssetMap} from '../../render/asset-map/build-asset-map.js';
 import {loadRootConfig} from '../load-config.js';
 import {
-  copyDir,
   isDirectory,
   isJsFile,
   loadJson,
@@ -16,7 +15,7 @@ import {
   writeFile,
 } from '../../core/fsutils.js';
 import {htmlMinify} from '../../render/html-minify.js';
-import {dim} from 'kleur/colors';
+import {dim, blue, cyan} from 'kleur/colors';
 import {getVitePlugins} from '../../core/plugin.js';
 import {getElements} from '../../core/elements.js';
 
@@ -174,15 +173,17 @@ export async function build(rootProjectDir?: string, options?: BuildOptions) {
     makeDir(buildDir);
   }
 
+
   // Copy files from `dist/client/{assets,chunks}` to `dist/html` using the
   // root manifest. Ignore route files.
+  console.log('\njs/css output:');
   makeDir(path.join(buildDir, 'assets'));
   makeDir(path.join(buildDir, 'chunks'));
   Object.keys(rootManifest).forEach((src) => {
     // Don't expose route files in the final output. If any client-side code
     // relies on route dependencies, it should probably be broken out into a
     // shared component instead.
-    if (src.startsWith('routes/')) {
+    if (isRouteFile(src)) {
       return;
     }
     const assetData = rootManifest[src];
@@ -190,6 +191,7 @@ export async function build(rootProjectDir?: string, options?: BuildOptions) {
     const assetFrom = path.join(distDir, 'client', assetRelPath);
     const assetTo = path.join(buildDir, assetRelPath);
     fsExtra.copySync(assetFrom, assetTo);
+    printFileOutput(fileSize(assetTo), 'dist/html/', assetRelPath);
   });
 
   // Pre-render HTML pages (SSG).
@@ -200,6 +202,7 @@ export async function build(rootProjectDir?: string, options?: BuildOptions) {
     const renderer = new render.Renderer(rootConfig, {assetMap});
     const sitemap = await renderer.getSitemap();
 
+    console.log('\nhtml output:');
     await Promise.all(
       Object.keys(sitemap).map(async (urlPath) => {
         const {route, params} = sitemap[urlPath];
@@ -209,18 +212,46 @@ export async function build(rootProjectDir?: string, options?: BuildOptions) {
 
         // The renderer currently assumes that all paths serve HTML.
         // TODO(stevenle): support non-HTML routes using `routes/[name].[ext].ts`.
-        let outPath = path.join(distDir, `html${urlPath}`, 'index.html');
-
-        if (outPath.endsWith('404/index.html')) {
-          outPath = outPath.replace('404/index.html', '404.html');
+        let outFilePath = path.join(urlPath.slice(1), 'index.html');
+        if (outFilePath.endsWith('404/index.html')) {
+          outFilePath = outFilePath.replace('404/index.html', '404.html');
         }
+        const outPath = path.join(buildDir, outFilePath);
 
         const html = await htmlMinify(data.html || '');
         await writeFile(outPath, html);
 
-        const relPath = outPath.slice(path.dirname(distDir).length + 1);
-        console.log(`saved ${relPath}`);
+        printFileOutput(fileSize(outPath), 'dist/html/', outFilePath);
       })
     );
   }
+}
+
+function isRouteFile(filepath: string) {
+  return filepath.startsWith('routes') && isJsFile(filepath);
+}
+
+function fileSize(filepath: string) {
+  const stats = fsExtra.statSync(filepath);
+  const bytes = stats.size;
+
+  const k = 1024;
+  if (bytes < k) {
+    return (bytes / k).toFixed(2) + ' kB';
+  }
+  const units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + units[i];
+}
+
+function printFileOutput(
+  fileSize: string,
+  outputDir: string,
+  outputFile: string
+) {
+  const indent = ' '.repeat(2);
+  const paddedSize = fileSize.padStart(9, ' ');
+  console.log(
+    `${indent}${dim(paddedSize)}  ${dim(outputDir)}${cyan(outputFile)}`
+  );
 }
