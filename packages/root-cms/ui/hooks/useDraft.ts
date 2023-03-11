cb0d43d6573507c3e5744af75d03dffd448e51cc
Steven Le <stevenle08@gmail.com>
2023-03-10 20:31:44 -0800

    feat: prioritize local changes over remote updates

--- packages/root-cms/ui/hooks/useDraft.ts
+++ packages/root-cms/ui/hooks/useDraft.ts
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'preact/hooks';
+import {useEffect, useMemo, useState} from 'preact/hooks';
 import {
   doc,
   DocumentReference,
@@ -6,8 +6,11 @@ import {
   onSnapshot,
   updateDoc,
   Timestamp,
+  deleteField,
 } from 'firebase/firestore';
 import {debounce} from '../utils/debounce.js';
+
+const SAVE_DELAY_MS = 3 * 1000;
 
 export class DraftController {
   readonly projectId: string;
@@ -44,7 +47,13 @@ export class DraftController {
     console.log('start()');
     this.dbUnsubscribe = onSnapshot(this.docRef, (snapshot) => {
       const data = snapshot.data();
-      console.log('onSnapshot()', data);
+      console.log(this.pendingUpdates);
+      // Save the user's local changes to the snapshot so that their updates
+      // are not overwritten.
+      if (Object.keys(this.pendingUpdates).length > 0) {
+        applyUpdates(data, this.pendingUpdates);
+      }
+      console.log('onSnapshot()', data, this.pendingUpdates);
       if (this.onChangeCallback) {
         this.onChangeCallback(data);
       }
@@ -69,13 +78,19 @@ export class DraftController {
   }
 
   /**
-   * Updates a key.
+   * Updates a key. The key can be a nested key, e.g. "meta.title".
    */
   async updateKey(key: string, newValue: any) {
     console.log('updateKey()', key, newValue);
     this.pendingUpdates[`fields.${key}`] = newValue;
-    // await updateDoc(this.docRef, `fields.${key}`, newValue);
     this.queueChanges();
+  }
+
+  /**
+   * Removes a key.
+   */
+  async removeKey(key: string) {
+    this.updateKey(key, deleteField());
   }
 
   /**
@@ -83,7 +98,7 @@ export class DraftController {
    * number of times, and the write will go through N seconds after the last
    * update.
    */
-  private queueChanges = debounce(() => this.flush(), 3000);
+  private queueChanges = debounce(() => this.flush(), SAVE_DELAY_MS);
 
   /**
    * Immediately write all queued data to the DB.
@@ -105,10 +120,30 @@ export class DraftController {
   }
 }
 
+function applyUpdates(data: any, updates: any) {
+  for (const key in updates) {
+    const val = updates[key];
+    if (key.includes('.')) {
+      const [head, tail] = splitKey(key);
+      data[head] ??= {};
+      applyUpdates(data[head], {[tail]: val});
+    } else {
+      data[key] = val;
+    }
+  }
+}
+
+function splitKey(key: string) {
+  const index = key.indexOf('.');
+  const head = key.substring(0, index);
+  const tail = key.substring(index + 1);
+  return [head, tail] as const;
+}
+
 export function useDraft(docId: string) {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState<any>({});
-  const draft = new DraftController(docId);
+  const draft = useMemo(() => new DraftController(docId), []);
 
   useEffect(() => {
     draft.onChange((data: any) => {
