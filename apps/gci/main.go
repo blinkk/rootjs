388e097457d4eddae757834c6513f5f378c61e8a
Steven Le <stevenle08@gmail.com>
2023-04-04 06:16:20 -0700

    fix: fix gci handling of .copy files

--- apps/gci/main.go
+++ apps/gci/main.go
@@ -34,14 +34,16 @@ func servingURLHandler(w http.ResponseWriter, r *http.Request) {
 	servingUrl, err := getServingURL(gcsPath)
 	if err != nil {
 		log.Printf("failed to get serving url: %v", err)
-		fmt.Fprint(w, "{\"success\" false}")
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, "{\"success\": false}")
 		return
 	}
 	resData := ServingURLData{Success: true, ServingURL: servingUrl}
 	data, err := json.Marshal(resData)
 	if err != nil {
 		log.Printf("failed to serialize json: %v", err)
-		fmt.Fprint(w, "{\"success\" false}")
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, "{\"success\": false}")
 		return
 	}
 	w.Write(data)
@@ -57,9 +59,10 @@ func getServingURL(gcsPath string) (string, error) {
 	url, err := image.ServingURL(ctx, blobkey, &image.ServingURLOptions{Secure: true})
 	if err != nil {
 		log.Printf("failed to get image serving url: %v", err)
-		if !strings.HasSuffix(gcsPath, ".copy") {
+		if !strings.Contains(gcsPath, ".copy") {
 			copyGcsPath, err := cloneGCSFile(gcsPath)
 			if err != nil {
+				log.Printf("failed to clone %s: %v", gcsPath, err)
 				return "", err
 			}
 			return getServingURL(copyGcsPath)
@@ -76,14 +79,30 @@ func cloneGCSFile(gcsPath string) (string, error) {
 		return "", err
 	}
 	parts := strings.Split(gcsPath, "/")
-	bucketName := parts[2]
-	fileName := strings.Join(parts[3:], "/")
+	bucketName := parts[1]
+	srcPath := strings.Join(parts[2:], "/")
+	dstPath := addCopyToFilename(srcPath)
 	bucket := gcsClient.Bucket(bucketName)
-	src := bucket.Object(fileName)
-	dst := bucket.Object(fileName + ".copy")
+	src := bucket.Object(srcPath)
+	dst := bucket.Object(dstPath)
 	_, err = dst.CopierFrom(src).Run(ctx)
-	newGcsPath := "/gs/" + bucketName + "/" + fileName + ".copy"
+	newGcsPath := "/" + bucketName + "/" + dstPath
+	log.Printf("copying to: %v", newGcsPath)
 	return newGcsPath, err
+}
+
+func addCopyToFilename(fileName string) string {
+	// Find the last occurrence of "." in the filename.
+	dotIndex := strings.LastIndex(fileName, ".")
+	// If the dot is not found, return the original filename with ".copy".
+	if dotIndex == -1 {
+		return fileName + ".copy"
+	}
+	// Split the filename into its name and extension parts.
+	name := fileName[:dotIndex]
+	ext := fileName[dotIndex:]
+	// Return the new filename with ".copy" added before the extension.
+	return name + ".copy" + ext
 }
 
 type ServiceAccountData struct {
@@ -102,14 +121,14 @@ func serviceAccountHandler(w http.ResponseWriter, r *http.Request) {
 	serviceAccount, err := appengine.ServiceAccount(ctx)
 	if err != nil {
 		log.Printf("failed to get service account: %v", err)
-		fmt.Fprint(w, "{\"success\" false}")
+		fmt.Fprint(w, "{\"success\": false}")
 		return
 	}
 	resData := ServiceAccountData{Success: true, ServiceAccount: serviceAccount}
 	data, err := json.Marshal(resData)
 	if err != nil {
 		log.Printf("failed to serialize json: %v", err)
-		fmt.Fprint(w, "{\"success\" false}")
+		fmt.Fprint(w, "{\"success\": false}")
 		return
 	}
 	w.Write(data)
