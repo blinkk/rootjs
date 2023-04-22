756f87276059fd1cf984627377030dc712816cc4
Steven Le <stevenle08@gmail.com>
2023-04-21 20:08:48 -0700

    fix: better logical sorting for 404 page

--- packages/root/src/core/pages/DevNotFoundPage.tsx
+++ packages/root/src/core/pages/DevNotFoundPage.tsx
@@ -7,26 +7,50 @@ interface DevNotFoundPageProps {
   sitemap: Record<string, {route: Route; params: Record<string, string>}>;
 }
 
+interface RoutesListMap {
+  [src: string]: Array<RouteURLPath>;
+}
+
+interface RouteURLPath {
+  route: Route;
+  urlPath: string;
+}
+
 export function DevNotFoundPage(props: DevNotFoundPageProps) {
   const req = props.req;
-  const routesList: Array<{src: string; urlPath: string}> = [];
-  let urlMaxLength = 0;
+  const routesListMap: RoutesListMap = {};
+  let srcMaxLength = 0;
   Object.keys(props.sitemap).forEach((urlPath) => {
     const route = props.sitemap[urlPath].route;
-    routesList.push(Object.assign({}, route, {urlPath}));
-    if (urlPath.length > urlMaxLength) {
-      urlMaxLength = urlPath.length;
+    routesListMap[route.src] ??= [];
+    routesListMap[route.src].push({route, urlPath});
+    if (route.src.length > srcMaxLength) {
+      srcMaxLength = route.src.length;
     }
   });
-  const routesListString = routesList
-    .map((route) => {
-      return `${route.urlPath.padEnd(urlMaxLength, ' ')}  =>  ${route.src}`;
-    })
-    .join('\n');
+  const routeSrcs = Object.keys(routesListMap).sort(sortRouteFiles);
+  const lines: string[] = [];
+  routeSrcs.forEach((routeSrc) => {
+    const routeUrls = routesListMap[routeSrc].sort(sortRouteURLs);
+    routeUrls.forEach((routeUrl, i) => {
+      const urlPath = routeUrl.urlPath;
+      if (i === 0) {
+        lines.push(`${routeSrc.padEnd(srcMaxLength, ' ')}  =>  ${urlPath}`);
+      } else {
+        lines.push(`${''.padEnd(srcMaxLength, ' ')}  =>  ${urlPath}`);
+      }
+    });
+  });
+  const routesListString = lines.join('\n');
+  // const routesListString = routesList
+  //   .map((route) => {
+  //     return `${route.urlPath.padEnd(srcMaxLength, ' ')}  =>  ${route.src}`;
+  //   })
+  //   .join('\n');
   return (
     <ErrorPage code={404} title="Not found">
       <h2>Routes</h2>
-      {routesList.length > 0 ? (
+      {Object.keys(routesListMap).length > 0 ? (
         <pre className="box">
           <code>{routesListString}</code>
         </pre>
@@ -42,4 +66,24 @@ export function DevNotFoundPage(props: DevNotFoundPageProps) {
       </pre>
     </ErrorPage>
   );
+}
+
+function sortRouteFiles(a: string, b: string): number {
+  if (a === 'routes/index.tsx') {
+    return -1;
+  }
+  if (b === 'routes/index.tsx') {
+    return 1;
+  }
+  return a.localeCompare(b);
+}
+
+function sortRouteURLs(a: RouteURLPath, b: RouteURLPath): number {
+  if (a.route.isDefaultLocale && !b.route.isDefaultLocale) {
+    return -1;
+  }
+  if (!a.route.isDefaultLocale && b.route.isDefaultLocale) {
+    return 1;
+  }
+  return a.urlPath.localeCompare(b.urlPath);
 }
