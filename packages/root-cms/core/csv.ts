404b39799744f69625df9e2eb302b460474d0728
Steven Le <stevenle08@gmail.com>
2023-04-24 17:16:58 -0700

    refactor: switch from csv-writer to csv-stringify

--- packages/root-cms/core/csv.ts
+++ packages/root-cms/core/csv.ts
@@ -1,11 +1,4 @@
-import {createRequire} from 'node:module';
-const require = createRequire(import.meta.url);
-
-// NOTE(stevenle): the csv-writer package has misconfigured "types" so `require`
-// is needed here.
-const createObjectCsvStringifier =
-  require('csv-writer').createObjectCsvStringifier;
-// import type {createObjectCsvStringifier} from 'csv-writer';
+import {stringify as csvStringify} from 'csv-stringify/sync';
 
 export function arrayToCsv(req: {
   headers: string[];
@@ -13,14 +6,8 @@ export function arrayToCsv(req: {
 }): string {
   const headers = req.headers || [];
   const rows = req.rows || [];
-  if (headers.length === 0 || rows.length === 0) {
-    return '';
-  }
-
-  const csvWriter = createObjectCsvStringifier({
-    header: headers.map((h) => ({id: h, title: h})),
+  return csvStringify(rows, {
+    header: true,
+    columns: headers,
   });
-  const csvHeader = csvWriter.getHeaderString() || '';
-  const csvRows = csvWriter.stringifyRecords(rows) || '';
-  return csvHeader + csvRows;
 }
