95e98a6c040837bd17f94968aefcbcc3fd69aa8c
Steven Le <stevenle08@gmail.com>
2024-03-29 14:02:35 -0700

    fix: fix externalize-deps

--- packages/root/src/node/load-config.ts
+++ packages/root/src/node/load-config.ts
@@ -51,7 +51,7 @@ export async function bundleRootConfig(rootDir: string, outPath: string) {
         setup(build) {
           build.onResolve({filter: /.*/}, (args) => {
             const id = args.path;
-            if (id[0] !== '@' && id[0] !== '.' && !path.isAbsolute(id)) {
+            if (id[0] !== '.' && !path.isAbsolute(id)) {
               return {
                 external: true,
               };
