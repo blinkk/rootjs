3049f8cac94c25e042fd1e6653e22a54546a8686
Steven Le <stevenle08@gmail.com>
2024-09-24 11:50:40 -0700

    feat: add plugin handle404 middleware

--- packages/root/src/cli/preview.ts
+++ packages/root/src/cli/preview.ts
@@ -106,6 +106,13 @@ export async function createPreviewServer(options: {
       // Add the root.js preview server middlewares.
       server.use(rootPreviewServerMiddleware());
 
+      // Add any custom plugin 404 handlers.
+      plugins.forEach((plugin) => {
+        if (plugin.handle404) {
+          server.use(plugin.handle404);
+        }
+      });
+
       // Add error handlers.
       server.use(rootPreviewServer404Middleware());
       server.use(rootPreviewServer500Middleware());
