890485f98f82de594c501fb6e1841cfe8998eb8f
Steven Le <s@blinkk.com>
2024-10-17 12:18:43 -0700

    feat: [v2] add cmsRoute() for defining ssg/ssr handlers (#435)

--- packages/root-cms/core/core.ts
+++ packages/root-cms/core/core.ts
@@ -1,3 +1,4 @@
 export * from './client.js';
+export * from './cms-route.js';
 export * from './runtime.js';
 export * as schema from './schema.js';
--- packages/root-cms/core/plugin.ts
+++ packages/root-cms/core/plugin.ts
@@ -1,8 +1,7 @@
 import {promises as fs} from 'node:fs';
 import path from 'node:path';
 import {fileURLToPath} from 'node:url';
-
-import {
+import type {
   ConfigureServerOptions,
   NextFunction,
   Plugin,
