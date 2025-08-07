e6e9fdf264804d3c1f592d7295af21422204a73a
Jeremy Weinstein <jeremydw@gmail.com>
2025-08-07 09:52:26 -0700

    chore: tidy up gci deployment

--- apps/gci/deploy.sh
+++ apps/gci/deploy.sh
@@ -1,5 +1,32 @@
 #!/bin/sh
+#
+# Deploys the Google Cloud Images microservice to Google App Engine.
+#
+# USAGE:
+#   ./deploy.sh [options]
+#
+# OPTIONS:
+#   --project      The GCP project (default: rootjs-gci)
+#
+# EXAMPLES:
+#   ./deploy.sh
+#   ./deploy.sh --project my-custom-project
 
 set -e
 
-gcloud app deploy --project=rootjs-gci --version=prod --promote app.yaml
+PROJECT="rootjs-gci"
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --project)
+      PROJECT="$2"
+      shift 2
+      ;;
+    *)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
+
+gcloud app deploy --project="$PROJECT" --version=prod --promote -q app.yaml
