db84916c445ba88d190ed3000faecafd1da98e3e
Steven Le <s@blinkk.com>
2025-09-11 21:06:01 -0700

    chore: simplify file field state logic (#741)
    Also:
    - Alias the ImageField to FileField
    - Add hook for useDraftDocValue() which has a return value similar to
      useState() but automatically subscribes to changes to the db

--- packages/root-cms/ui/components/DocEditor/fields/ImageField.tsx
+++ packages/root-cms/ui/components/DocEditor/fields/ImageField.tsx
@@ -1,33 +1,6 @@
-import {useState} from 'preact/hooks';
-import * as schema from '../../../../core/schema.js';
-import {useDraftDoc, useDraftDocField} from '../../../hooks/useDraftDoc.js';
-import {UploadedFile} from '../../../utils/gcs.js';
-import {FileUploadField} from '../../FileUploadField/FileUploadField.js';
 import {FieldProps} from './FieldProps.js';
+import {FileField} from './FileField.js';
 
 export function ImageField(props: FieldProps) {
-  const field = props.field as schema.ImageField;
-  const [uploadedFile, setUploadedFile] = useState<UploadedFile | null>(null);
-  const draft = useDraftDoc().controller;
-
-  useDraftDocField(props.deepKey, (newUploadedFile: UploadedFile | null) => {
-    setUploadedFile(newUploadedFile);
-  });
-
-  return (
-    <FileUploadField
-      file={uploadedFile}
-      variant="image"
-      alt={field.alt}
-      exts={field.exts}
-      onFileChange={(file) => {
-        if (file) {
-          draft.updateKey(props.deepKey, file);
-        } else {
-          draft.removeKey(props.deepKey);
-        }
-        setUploadedFile(file);
-      }}
-    />
-  );
+  return <FileField {...props} variant="image" />;
 }
--- packages/root-cms/ui/components/DocEditor/fields/NumberField.tsx
+++ packages/root-cms/ui/components/DocEditor/fields/NumberField.tsx
@@ -1,32 +1,15 @@
 import {NumberInput} from '@mantine/core';
-import {useCallback, useState} from 'preact/hooks';
-import * as schema from '../../../../core/schema.js';
-import {useDraftDoc, useDraftDocField} from '../../../hooks/useDraftDoc.js';
+import {useDraftDocValue} from '../../../hooks/useDraftDoc.js';
 import {FieldProps} from './FieldProps.js';
 
 export function NumberField(props: FieldProps) {
-  const field = props.field as schema.NumberField;
-  const [value, setValue] = useState(field.default || 0);
-  const draft = useDraftDoc().controller;
-
-  const onChange = useCallback(
-    (newValue: number) => {
-      setValue(newValue);
-      draft.updateKey(props.deepKey, newValue);
-    },
-    [props.deepKey]
-  );
-
-  useDraftDocField(props.deepKey, (newValue: number) => {
-    setValue(newValue);
-  });
-
+  const [value, setValue] = useDraftDocValue(props.deepKey, 0);
   return (
     <NumberInput
       size="xs"
       radius={0}
       value={value}
-      onChange={(value: number) => onChange(value)}
+      onChange={(value: number) => setValue(value)}
       hideControls
     />
   );
