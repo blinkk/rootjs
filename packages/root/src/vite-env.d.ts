eba954344802fedbbdbd212e7e4be9a1c5e57967
Jeremy Weinstein <jeremydw@gmail.com>
2025-12-23 11:42:14 -0800

    feat: allow plugins to define routes

--- packages/root/src/vite-env.d.ts
+++ packages/root/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+declare module 'virtual:root-plugin-routes' {
+  const routes: Record<
+    string,
+    {module: import('./core/types.js').RouteModule; src: string}
+  >;
+  export default routes;
+}
