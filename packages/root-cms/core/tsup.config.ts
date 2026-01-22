de218010d450bdb6078722ac41eaa662d8a42c5a
Jeremy Weinstein <jeremydw@gmail.com>
2026-01-22 08:57:43 -0800

    fix pnpm test

--- packages/root-cms/core/tsup.config.ts
+++ packages/root-cms/core/tsup.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
     ],
   },
   format: ['esm'],
-  splitting: false,
+  splitting: true,
   platform: 'node',
   esbuildOptions(options) {
     options.tsconfig = './core/tsconfig.json';
