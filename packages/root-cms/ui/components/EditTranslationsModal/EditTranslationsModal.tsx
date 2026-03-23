93def92458d9648b97743bc77eaff64f5e120e07
Jeremy Weinstein <jeremydw@gmail.com>
2026-03-23 10:02:40 -0700

    feat: update button label from 'Source History' to 'History' in EditTranslationsModal

--- packages/root-cms/ui/components/EditTranslationsModal/EditTranslationsModal.tsx
+++ packages/root-cms/ui/components/EditTranslationsModal/EditTranslationsModal.tsx
@@ -290,7 +290,7 @@ export function EditTranslationsModal(
                 )}
                 onClick={() => setActiveTab('history')}
               >
-                Source History
+                History
               </button>
             </div>
           )}
